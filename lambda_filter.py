ca577221692ae2da00eb6ad0564c364dcd86cb0a
ItsMeSwaron <swaron3214@gmail.com>
2025-10-15 22:25:56 +0600

    filter and lambda problems solved

---
+++ lambda_filter.py
@@ -0,0 +1,16 @@
+"""
+Problem 3: Word Length Filter (Lambda + Filter)
+Problem:
+ Given a list of words, use filter() and lambda to return only words whose length is greater than 4.
+ Example Input:
+words = ["sun", "planet", "moon", "star", "universe"]
+
+Expected Output:
+['planet', 'universe']
+
+"""
+words = ["sun", "planet", "moon", "star", "universe"]
+
+ans = list(filter(lambda x : len(x) > 4, words))
+
+print(ans)
